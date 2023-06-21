7b575740fa52f33c84e99fa95f51d966b75bccdf
btconway <ben@vntana.com>
2023-06-21 12:10:53 -0500

    Update database.py

--- server/database.py
+++ server/database.py
@@ -45,13 +45,13 @@ def init_db():
     Create the schema for the database if it doesn't exist yet
     """
     client = get_client()
-    class_name = SCHEMA["class"]
 
-    try:
-        client.schema.get_class(class_name)
-        logging.debug(f"Schema for {class_name} already exists")
-        logging.debug("Skipping schema creation")
-    except weaviate.exceptions.ClassNotFound:
+    if not client.schema.contains(SCHEMA["class"]):
         logging.debug("Creating schema")
         client.schema.create_class(SCHEMA)
+    else:
+        class_name = SCHEMA["class"]
+        logging.debug(f"Schema for {class_name} already exists")
+        logging.debug("Skipping schema creation")
+
 

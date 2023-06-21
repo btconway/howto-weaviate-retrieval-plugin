3f7209c85f50861808f8f1dedd166b63a2c733a6
btconway <ben@vntana.com>
2023-06-21 11:01:46 -0500

    Update database.py

--- server/database.py
+++ server/database.py
@@ -12,14 +12,32 @@ SCHEMA = {
     ],
 }
 
+def _is_wcs_domain(url: str) -> bool:
+    """
+    Check if the given URL ends with ".weaviate.network" or ".weaviate.network/".
+    """
+    pattern = r"\.(weaviate\.cloud|weaviate\.network)(/)?$"
+    return bool(re.search(pattern, url))
+
+def _build_auth_credentials():
+    url = os.environ.get("WEAVIATE_URL", "https://localhost:8080")
+
+    if _is_wcs_domain(url):
+        api_key = os.environ.get("WEAVIATE_API_KEY")
+        if api_key is not None:
+            return weaviate.auth.AuthApiKey(api_key=api_key)
+        else:
+            raise ValueError("WEAVIATE_API_KEY environment variable is not set")
+    else:
+        return None
 
 def get_client():
     """
     Get a client to the Weaviate server
     """
     host = os.environ.get("WEAVIATE_HOST", "http://localhost:8080")
-    return weaviate.Client(host)
-
+    auth_credentials = _build_auth_credentials()
+    return weaviate.Client(host, auth_client_secret=auth_credentials)
 
 def init_db():
     """
